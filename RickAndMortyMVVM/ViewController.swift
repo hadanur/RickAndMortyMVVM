cd7346000e85109226873a069d619c1378006a25
hadanur <115171695+hadanur@users.noreply.github.com>
2022-11-12 13:14:11 +0300

    Initial Commit

---
+++ RickAndMortyMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyMVVM
+//
+//  Created by Hakan Adanur on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
